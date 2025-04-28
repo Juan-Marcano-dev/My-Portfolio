ee8e495f0778783b30c7574f67d365c5a8862680
Juan Marcano <jonkrms@gmail.com>
2025-04-28 19:24:52 -0400

    agregando menu desplegable

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
-import { useState } from 'react'
-import './App.css'
-import HeaderNav from './Components/HeaderNav'
+import { useState } from "react";
+import "./App.css";
+import logo from "./assets/images/logo-inicio.png";
+
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const [openedNavBar, setOpenNavBar] = useState(false);
   return (
     <>
-      <main>
-        <header>
-          <HeaderNav />
-        </header>
-      </main>
+      <div className="App">
+        <Navbar color="dark" fixed="top" dark expand="md">
+          <NavbarBrand href="">
+            <NavbarBrand href="/" className="logo-text">
+              &lt;JCMR&gt;
+            </NavbarBrand>
+          </NavbarBrand>
+        <NavbarToggler aria-label="Abrir Menu" />
+        <Collapse > 
+        </Collapse>
+        
+
+
+        </Navbar>
+      </div>
     </>
-  )
+  );
 }
 
-
-export default App
+export default App;
